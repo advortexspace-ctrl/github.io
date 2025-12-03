e7abfe645f3fc6882adcc414dfb38117a604df2a
advortexspace-ctrl <advortex.space@gmail.com>
2025-12-03 10:58:13 +0100

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// Main JavaScript File for VortexSpace Website
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Initialize all components
+    initLoadingScreen();
+    initThemeToggle();
+    initNavigation();
+    initScrollProgress();
+    initTypingEffect();
+    initCounters();
